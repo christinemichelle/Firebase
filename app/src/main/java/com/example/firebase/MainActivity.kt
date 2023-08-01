5559a54868a9cdf178603eab699cc78f51db2393
Christinemichelle <christinekito39@gmail.com>
2023-08-01 16:36:09 +0300

    Linking

--- app/src/main/java/com/example/firebase/MainActivity.kt
+++ app/src/main/java/com/example/firebase/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.example.firebase
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var signup = findViewById<TextView>(R.id.textView11)
+        signup.setOnClickListener {
+            val intent = Intent(this,Signup::class.java)
+            startActivity(intent)
+        }
+
     }
 }
--- app/src/main/java/com/example/firebase/Signup.kt
+++ app/src/main/java/com/example/firebase/Signup.kt
@@ -1,11 +1,25 @@
 package com.example.firebase
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class Signup : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signup)
+
+
+        var name = findViewById<TextView>(R.id.tvName)
+        var email = findViewById<TextView>(R.id.gmail)
+        var contact = findViewById<TextView>(R.id.phone)
+        var password = findViewById<TextView>(R.id.password)
+        var button = findViewById<Button>(R.id.btnsignup)
+        button.setOnClickListener {
+
+        }
     }
 }
